cf748ac06f4b397cebca8ce7b186a5f8d33e0db4
nataliagwardjan <natalia.gwardjan@gmail.com>
2025-02-17 14:48:03 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import sys
 from setuptools import setup, find_packages
 
+
 # Load all requirements from requirements.txt
 def parse_requirements(filename):
     with open(filename, 'r') as file:
         return [line.strip() for line in file if line.strip() and not line.startswith('#')]
+
 
 # Python version
 python_version = sys.version.split(' ')[0]
@@ -13,7 +15,21 @@ setup(
     name='article_models_library',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=parse_requirements("requirements.txt"),
+    # install_requires=parse_requirements("requirements.txt"),
+    install_requires=[
+        'annotated-types',
+        'iniconfig',
+        'packaging',
+        'pluggy',
+        'pydantic',
+        'pydantic-settings',
+        'pydantic-core',
+        'pytest',
+        'python-dotenv',
+        'setuptools',
+        'typing_extensions',
+        'wheel'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
